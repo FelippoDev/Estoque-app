04ee1fe026f7a855a2ba2bc545d8910d213f7413
Luiz Felippo <coelho.luizfelippo@gmail.com>
2021-05-28 20:43:59 -0300

    adding the drop record function

--- inventario_app.py
+++ inventario_app.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk, messagebox
+from PIL import ImageTk, Image
 import psycopg2
 import smtplib
 from email.message import EmailMessage
@@ -100,98 +101,156 @@ my_Canvas.create_text(450, 205, text='Valor Atual:')
 update_valor = Entry(my_Canvas, width=25, borderwidth=2)
 my_Canvas.create_window(580, 205, window=update_valor, height=25, width=180)
 
+my_Canvas.create_text(570, 20, text='e-mail do fornecedor:')
+fornecedor = Entry(my_Canvas, borderwidth=2)
+my_Canvas.create_window(725, 20, window=fornecedor, height=25, width=180)
+
 
 def confirm_btn():
     global nome_produto, quantidade_produto, valor_produto, con, cur
     nome = nome_produto.get()
     qt = quantidade_produto.get()
     v = valor_produto.get()
+    try:
+        # Executando a query
+        cur.execute(
+            f"INSERT INTO produto (nome_produto, quantidade_produto, valor_produto) VALUES ('{nome}', {qt}, {v})")
 
-    # Executando a query
-    cur.execute(f"INSERT INTO produto (nome_produto, quantidade_produto, valor_produto) VALUES ('{nome}', {qt}, {v})")
+        nome_produto.delete(0, END)
+        quantidade_produto.delete(0, END)
+        valor_produto.delete(0, END)
 
-    nome_produto.delete(0, END)
-    quantidade_produto.delete(0, END)
-    valor_produto.delete(0, END)
-
-    # comitando o dado
-    con.commit()
+        # comitando o dado
+        con.commit()
+    except:
+        msg_box = messagebox.showinfo('confirm message', 'Necessário informar o produto!',
+                                      icon='warning')
 
 
 # Atualiza os dados selecionados pelo usuário
 def update_func():
     global cur, update_valor, update_quantidade, con, tree
-    # Pegando o número armazenado tree table
-    selected = tree.focus()
-    # Pegando os valores armazenados
-    values = tree.item(selected, 'values')
-    nome = values[1]
-    # Pegando o input
-    quantidade = update_quantidade.get()
-    update_quantidade.delete(0, END)
+    try:
+        # Pegando o número armazenado tree table
+        selected = tree.focus()
+        # Pegando os valores armazenados
+        values = tree.item(selected, 'values')
+        nome = values[1]
+        # Pegando o input
+        quantidade = update_quantidade.get()
+        update_quantidade.delete(0, END)
 
-    valor = update_valor.get()
-    update_valor.delete(0, END)
-    # Executando o update do dado
-    cur.execute(f"UPDATE produto SET quantidade_produto = {quantidade} WHERE nome_produto = '{nome}';")
+        valor = update_valor.get()
+        update_valor.delete(0, END)
+        # Executando o update do dado
+        cur.execute(f"UPDATE produto SET quantidade_produto = {quantidade} WHERE nome_produto = '{nome}';")
 
-    cur.execute(f"UPDATE produto SET valor_produto = {valor} WHERE nome_produto = '{nome}';")
+        cur.execute(f"UPDATE produto SET valor_produto = {valor} WHERE nome_produto = '{nome}';")
 
-    # commiting the update
-    con.commit()
+        # commiting the update
+        con.commit()
+    except:
+        msg_box = messagebox.showinfo('update message', 'Necessário informar o produto!',
+                                      icon='warning')
 
 
 # Checar os dados na base de dados e envia e-mail ao fornecer se a quantidade de produtos for igual a 0
 def check_func():
-    global cur
+    global cur, fornecedor
+
     situation = False
     produtos_inventario = []
     cur.execute('SELECT * FROM produto')
     rows = cur.fetchall()
     for r in rows:
-        print(r)
         if r[2] == 0:
             situation = True
             produtos_inventario.append(r[1])
-    msg = EmailMessage()
-    msg['Subject'] = "Reposição de estoque"
-    msg['From'] = 'felippoBeifong@gmail.com'
-    msg['To'] = 'pedronhk@gmail.com'
-    if situation:
-        msg.set_content(f"Produto necessário a reposição:{produtos_inventario[0]}")
+    try:
+        email = fornecedor.get()
+        msg = EmailMessage()
+        msg['Subject'] = "Reposição de estoque"
+        msg['From'] = 'felippoBeifong@gmail.com'
+        msg['To'] = f'{email}'
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login('felippoBeifong@gmail.com', 'beifongtophlinsuyin')
-        smtp.send_message(msg)
+        if situation:
+            msg.set_content(f"Produto necessário a reposição:{produtos_inventario[0]}")
 
-    if situation:
-        messagebox.showinfo('check message',
-                            f'O produto {produtos_inventario[0]} está em falta! Já foi enviado e-mail ao fornecedor.',
-                            icon='warning')
-    else:
-        messagebox.showinfo('check message', 'Sem produtos necessário a ser feito reposição.', icon='info')
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login('felippoBeifong@gmail.com', 'beifongtophlinsuyin')
+            smtp.send_message(msg)
+
+        if situation:
+            messagebox.showinfo('check message',
+                                f'O produto {produtos_inventario[0]} está em falta! Já foi enviado e-mail ao fornecedor.',
+                                icon='warning')
+        else:
+            messagebox.showinfo('check message', 'Sem produtos necessário a ser feito reposição.', icon='info')
+
+    except:
+        msg_box = messagebox.showinfo('check message', 'Necessário informar o e-mail antes do processo de checagem!',
+                                      icon='warning')
 
 
 def drop_func():
-    global cur
+    global cur, con
     delete = False
-    # Grab record data
-    selected = tree.focus()
-    # Grab record values
-    values = tree.item(selected, 'values')
-    nome = values[1]
-    msg_box = messagebox.askquestion('drop record', 'Certeza que deseja deletar este dado?', icon='warning')
-    if msg_box == 'yes':
-        delete = True
+    sel = True
+    try:
+        # Grab record data
+        selected = tree.focus()
+        # Grab record values
+        values = tree.item(selected, 'values')
+        id = values[0]
+    except:
+        msg_box = messagebox.showinfo('drop record', 'Necessário selecionar algum produto para retirar!',
+                                      icon='warning')
     else:
-        delete = False
-    if delete:
-        cur.execute(f'DELETE FROM produto WHERE nome_produto = {nome}')
+        msg_box = messagebox.askquestion('drop record', 'Certeza que deseja deletar este dado?', icon='warning')
+        if msg_box == 'yes':
+            delete = True
+        else:
+            delete = False
+        if delete:
+            cur.execute(f'DELETE FROM produto WHERE id = {id};')
+            con.commit()
+
+
+def refresh_func():
+    global tree
+    for i in tree.get_children():
+        tree.delete(i)
+    tree = ttk.Treeview(tree_frame)
+    tree.place(x=0, y=0, height=200)
+    tree['columns'] = ("ID", "Nome", "Quantidade", "Valor unitario")
+    tree.column("#0", width=0, stretch=NO)
+    tree.column("ID", anchor=CENTER, width=70, minwidth=25)
+    tree.column("Nome", anchor=CENTER, width=120, minwidth=25)
+    tree.column("Quantidade", anchor=CENTER, width=156, minwidth=25)
+    tree.column("Valor unitario", anchor=CENTER, width=120, minwidth=25)
+    tree.heading("#0", text="", anchor=CENTER)
+    tree.heading("ID", text="ID", anchor=CENTER)
+    tree.heading("Nome", text="Nome do Produto", anchor=CENTER)
+    tree.heading("Quantidade", text="Quantidade em estoque", anchor=CENTER)
+    tree.heading("Valor unitario", text="Valor Unitário", anchor=CENTER)
+    tree.tag_configure('oddrow', background="white")
+    tree.tag_configure('evenrow', background="#4b0082")
+    cur.execute('SELECT * FROM produto ORDER BY ID')
+    rows = cur.fetchall()
+    count = 0
+    for r in rows:
+        if count % 2 == 0:
+            tree.insert(parent='', index='end', iid=count, text='Parent', values=(r[0], r[1], r[2], r[3]),
+                        tags=('evenrow',))
+        else:
+            tree.insert(parent='', index='end', iid=count, text='Parent', values=(r[0], r[1], r[2], r[3]),
+                        tags=('oddrow',))
+        count += 1
 
 
 # Botão de checagem de estoque
-check = Button(my_Canvas, text='check', command=check_func)
-check.place(x=720, y=260)
+check = Button(my_Canvas, text='checar dados', command=check_func)
+check.place(x=720, y=45)
 
 # Botão de confirmação
 confirm = Button(my_Canvas, text='confirmar', command=confirm_btn)
@@ -202,6 +261,16 @@ update = Button(my_Canvas, text='update', command=update_func)
 update.place(x=680, y=205)
 
 # Botão de retirada de item na base de dados
-drop = Button(my_Canvas, image='drop', command=drop_func)
+img1 = Image.open('trash.png')
+img1_resized = img1.resize((20, 20), Image.ANTIALIAS)
+img1 = ImageTk.PhotoImage(img1_resized)
+drop = Button(my_Canvas, image=img1, command=drop_func, borderwidth=0)
+drop.place(x=600, y=400)
 
+# Refresh button
+img2 = Image.open('refresh.png')
+img2_resized = img2.resize((20, 20), Image.ANTIALIAS)
+img2 = ImageTk.PhotoImage(img2_resized)
+refresh = Button(my_Canvas, image=img2, command=refresh_func, borderwidth=0)
+refresh.place(x=600, y=350)
 mainloop()
